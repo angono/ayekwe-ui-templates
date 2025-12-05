841eb660a63fed97390cc3c16a047d37facc7187
angono <tirelangono@gmail.com>
2025-12-05 14:29:22 +0300

    Update UI

---
+++ node/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // '../**/*.html',
+    // '../**/**/*.html',
+    '../*.html',
+    '*.html',
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
